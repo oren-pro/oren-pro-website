1c6888130db0091b778793d30dae599ff819a014
oren-pro <mosh.kainer@gmail.com>
2018-08-13 19:16:22 +0300

    cloudinary test 2

--- src/components/homepage/HomePageCarousel.js
+++ src/components/homepage/HomePageCarousel.js
@@ -3,19 +3,19 @@ import UncontrolledCarousel from '../UncontrolledCarousel';
 
 const itemsDesktop = [
   {
-    src: 'http://res.cloudinary.com/dccqlnk3t/image/upload/v1534116622/carousel_01_lsqx0l.jpg',
+    src: 'https://res.cloudinary.com/dccqlnk3t/image/upload/v1534116622/carousel_01_lsqx0l.jpg',
     altText: 'Slide 1',
     caption: '',
     header: ''
   },
   {
-    src: 'http://res.cloudinary.com/dccqlnk3t/image/upload/v1534116644/carousel_02_slwqcc.jpg',
+    src: 'https://res.cloudinary.com/dccqlnk3t/image/upload/v1534116644/carousel_02_slwqcc.jpg',
     altText: 'Slide 2',
     caption: '',
     header: ''
   },
   {
-    src: 'http://res.cloudinary.com/dccqlnk3t/image/upload/v1534141001/carousel_03_a9a2uo.jpg',
+    src: 'https://res.cloudinary.com/dccqlnk3t/image/upload/v1534141001/carousel_03_a9a2uo.jpg',
     altText: 'Slide 3',
     caption: '',
     header: ''
