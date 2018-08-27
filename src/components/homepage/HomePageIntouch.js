ac6df5a60dbd9fe36b40e1ff6b685bae929d2abb
oren-pro <mosh.kainer@gmail.com>
2018-08-27 12:36:11 +0300

    edge fix 7

--- src/components/homepage/HomePageIntouch.js
+++ src/components/homepage/HomePageIntouch.js
@@ -12,9 +12,9 @@ export class HomePageIntouch extends React.Component {
     render() {
         return (
             <div className="homepage__intouch__box">
-                <div style={{width: '100%'}}>
-                <img className="desktop" style={{width: '100%'}} src="/images/homepage/intouch/intouch-border.png" />
-                </div>
+
+                <div className="common__intouch__seperator__desktop"></div>
+
                 <img className="homepage__intouch__header desktop_inline" alt="הישארו מעודכנים" src="/images/homepage/intouch/intouch-header.svg" />
                 <img className="homepage__intouch__header mobile" alt="הישארו מעודכנים" src="/images/homepage/intouch/intouch-header-mobile.svg" />
                 <p className="homepage__intouch__text Heebo-Regular" dir="rtl">הירשמו לניוזלטר שלנו וקבלו עדכונים על הדברים הכי חמים שיש </p>
