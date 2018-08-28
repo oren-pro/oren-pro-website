59f3d3f97c793422e608c55368b07469c1dd3399
oren-pro <mosh.kainer@gmail.com>
2018-08-28 09:24:13 +0300

    mail + newsletter 4

--- src/actions/newsletter.js
+++ src/actions/newsletter.js
@@ -11,41 +11,41 @@ export const subscribeToNewsletter = (newsletterData = {}) => {
 console.log(name);
 console.log(email);
 
-        const payload = {
-            "headers":{
-                'Content-Type': 'application/x-www-form-urlencoded',
-                "Access-Control-Allow-Origin": '*',
-                "Accept": 'application/json'
-            }
-        }
+        // const payload = {
+        //     "headers":{
+        //         'Content-Type': 'application/x-www-form-urlencoded',
+        //         "Access-Control-Allow-Origin": '*',
+        //         "Accept": 'application/json'
+        //     }
+        // }
 
 
 
-         var postData = {
-            firstname: name,
-            email: email,
-            apikey: "f6c20ab2-9ce5-403b-aeb3-b05c1f6b0af2",
-            successredirect: "https://oren-pro-website.herokuapp.com/newsletter_success",
-            failedredirect: "https://oren-pro-website.herokuapp.com/newsletter_failed",
-            viplists: "0",
-            exists: "merge",
-            restore: "restoreondeleted"
-        };
+        //  var postData = {
+        //     firstname: name,
+        //     email: email,
+        //     apikey: "f6c20ab2-9ce5-403b-aeb3-b05c1f6b0af2",
+        //     successredirect: "https://oren-pro-website.herokuapp.com/newsletter_success",
+        //     failedredirect: "https://oren-pro-website.herokuapp.com/newsletter_failed",
+        //     viplists: "0",
+        //     exists: "merge",
+        //     restore: "restoreondeleted"
+        // };
 
-        let axiosConfig = {
-            headers: {
-                'Content-Type': 'application/json;charset=UTF-8',
-                "Access-Control-Allow-Origin": "http://localhost:8080",
-            }
-        };
+        // let axiosConfig = {
+        //     headers: {
+        //         'Content-Type': 'application/json;charset=UTF-8',
+        //         "Access-Control-Allow-Origin": "http://localhost:8080",
+        //     }
+        // };
 
-        axios.post('http://api.viplus.com/gates/wsgate.asmx/RMembers_Import', postData, axiosConfig)
-        .then((res) => {
-            console.log("RESPONSE RECEIVED: ", res);
-        })
-        .catch((err) => {
-            console.log("AXIOS ERROR: ", err);
-        })
+        // axios.post('http://api.viplus.com/gates/wsgate.asmx/RMembers_Import', postData, axiosConfig)
+        // .then((res) => {
+        //     console.log("RESPONSE RECEIVED: ", res);
+        // })
+        // .catch((err) => {
+        //     console.log("AXIOS ERROR: ", err);
+        // })
 
 
 
@@ -82,44 +82,44 @@ console.log(email);
         // });
 
 
-//         function post(method) {
-//             method = method || "post"; // Set method to post by default if not specified.
-// console.log('in post');
+        function post(method) {
+            method = method || "post"; // Set method to post by default if not specified.
+            console.log('in post');
 
-//             const params = {
-//                 "firstname": name,
-//                 "email": email,
-//                 "apikey": "f6c20ab2-9ce5-403b-aeb3-b05c1f6b0af2",
-//                 "successredirect": "http://oren-pro-website.herokuapp.com",
-//                 "failedredirect": "http://oren-pro-website.herokuapp.com",
-//                 "viplists": "0",
-//                 "exists": "merge",
-//                 "restore": "restoreondeleted"
-//             }
+            const params = {
+                "firstname": name,
+                "email": email,
+                "apikey": "f6c20ab2-9ce5-403b-aeb3-b05c1f6b0af2",
+                "successredirect": "https://oren-pro-website.herokuapp.com",
+                "failedredirect": "https://oren-pro-website.herokuapp.com",
+                "viplists": "0",
+                "exists": "merge",
+                "restore": "restoreondeleted"
+            }
 
-//             // The rest of this code assumes you are not using a library.
-//             // It can be made less wordy if you use one.
-//             var form = document.createElement("form");
-//             form.setAttribute("method", method);
-//             form.setAttribute("action", "http://api.viplus.com/gates/wsgate.asmx/RMembers_Import");
+            // The rest of this code assumes you are not using a library.
+            // It can be made less wordy if you use one.
+            var form = document.createElement("form");
+            form.setAttribute("method", method);
+            form.setAttribute("action", "http://api.viplus.com/gates/wsgate.asmx/RMembers_Import");
 
-//             for(var key in params) {
-//                 if(params.hasOwnProperty(key)) {
-//                     var hiddenField = document.createElement("input");
-//                     hiddenField.setAttribute("type", "hidden");
-//                     hiddenField.setAttribute("name", key);
-//                     hiddenField.setAttribute("value", params[key]);
+            for(var key in params) {
+                if(params.hasOwnProperty(key)) {
+                    var hiddenField = document.createElement("input");
+                    hiddenField.setAttribute("type", "hidden");
+                    hiddenField.setAttribute("name", key);
+                    hiddenField.setAttribute("value", params[key]);
 
-//                     form.appendChild(hiddenField);
-//                 }
-//             }
+                    form.appendChild(hiddenField);
+                }
+            }
 
-//             document.body.appendChild(form);
-//             form.submit();
-//         }
+            document.body.appendChild(form);
+            form.submit();
+        }
 
 
-//         post();
+        post();
 
 
 
