e821e4f5859be9d83f7b793b2bc4dc282a574cfa
oren-pro <mosh.kainer@gmail.com>
2018-08-28 13:01:23 +0300

    mail + newsletter 16

--- server/server.js
+++ server/server.js
@@ -107,7 +107,7 @@ app.post("/sendEmail", bodyParser.urlencoded(), function(request, response) {
         sandbox: true
         },
         content: {
-        from: 'testing@zzz.com',// + process.env.SPARKPOST_SANDBOX_DOMAIN, // 'testing@sparkpostbox.com'
+        from: 'messeging@' + process.env.SPARKPOST_SANDBOX_DOMAIN, // 'testing@sparkpostbox.com'
         subject: 'Oh hey!',
         html:'<html><body><p>Testing SparkPost - the world\'s most awesomest email service!</p></body></html>'
         },
