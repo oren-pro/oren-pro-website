b3a9e5201bc6c5ee5cde5848c9607452c693bae6
oren-pro <mosh.kainer@gmail.com>
2018-08-31 10:55:36 +0300

    cleanup 3 webpack config 6

--- webpack.config.js
+++ webpack.config.js
@@ -56,29 +56,10 @@ module.exports = (env) => {
                 'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
                 'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
             }),
-            new webpack.DefinePlugin({ //<--key to reduce React's size
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-            }),
-            new webpack.optimize.DedupePlugin(),
-            new webpack.optimize.UglifyJsPlugin(),
-            new webpack.optimize.AggressiveMergingPlugin(),
-            new CompressionPlugin({
-            asset: "[path].gz[query]",
-            algorithm: "gzip",
-            test: /\.js$|\.css$|\.html$/,
-            threshold: 10240,
-            minRatio: 0.8
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'vendor'
             })
         ],
-        resolve: {
-            extensions: ['', '.js', '.jsx'],
-            "alias": {
-            "react": "preact-compat",
-            "react-dom": "preact-compat"
-            }
-        },
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
