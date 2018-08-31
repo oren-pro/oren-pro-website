89ded791fdbfe1490b5b4e23ac21389fb00dc899
oren-pro <mosh.kainer@gmail.com>
2018-08-31 10:41:51 +0300

    cleanup 3 webpack config 2

--- webpack.config.js
+++ webpack.config.js
@@ -54,10 +54,7 @@ module.exports = (env) => {
                 'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
                 'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
                 'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-                'process.env': {
-                    'NODE_ENV': JSON.stringify('production')
-                }
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
             }),
             new webpack.optimize.DedupePlugin(), //dedupe similar code 
             new webpack.optimize.UglifyJsPlugin(), //minify everything
