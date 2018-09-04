6ae191f531b5de04920c016b37df110ba9e22f5c
oren-pro <mosh.kainer@gmail.com>
2018-09-04 07:31:08 +0300

    ie carousel slide 3

--- src/components/homepage/HomePageCarousel.js
+++ src/components/homepage/HomePageCarousel.js
@@ -17,6 +17,24 @@ import {
 } from '../../actions/mobileGallery';
 
 
+(function () {
+
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
+
+
+
 class HomePageCarousel extends React.Component {
     constructor(props) {
         super(props);
