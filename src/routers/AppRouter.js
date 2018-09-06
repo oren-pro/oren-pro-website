16ca20dee3d7e0f890aae9b360bc54b76160e168
oren-pro <mosh.kainer@gmail.com>
2018-09-06 17:24:45 +0300

    1 page test

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -56,11 +56,11 @@ if (navigator.userAgent.toLowerCase().indexOf('msie') > -1 || navigator.userAgen
 //   }
 // });
 
-import AboutPage from '../containers/AboutPage';
-import ContactPage from '../containers/ContactPage';
-import DifferentPage from '../containers/DifferentPage';
-import EventsPage from '../containers/EventsPage';
-import EventPage from '../containers/EventPage';
+// import AboutPage from '../containers/AboutPage';
+// import ContactPage from '../containers/ContactPage';
+// import DifferentPage from '../containers/DifferentPage';
+// import EventsPage from '../containers/EventsPage';
+// import EventPage from '../containers/EventPage';
 import HomePage from '../containers/HomePage';
 import NotFoundPage from '../containers/NotFoundPage';
 import LoginPage from '../components/LoginPage';
@@ -83,25 +83,7 @@ class AppRouter extends React.Component {
                             this.props.events.categories ?
                             <Switch>
                                 <Route path="/" render={(props) => ( <HomePage {...props} />)} exact={true} />
-                                <Route path="/about" component={AboutPage} exact={true} />
-                                <Route path="/contact" component={ContactPage} exact={true} />
-                            
-                                    
-                                {
-                                    this.props.events.categories.map((category, index) => {
-                                        return <Route path={`/${category.name.replace(" ", "_").replace(" ", "_").replace(" ", "_").replace(" ", "_")}`} key={category.id} render={(props) => ( <EventsPage {...props} category={category} categoryIndex={index} />)} exact={true} />;
-                                    })
-                                }
-                                {
-                                    this.props.events.categories.map((category) => {
-                                        return <Route path={`/:event/${category.name.replace(" ", "_").replace(" ", "_").replace(" ", "_").replace(" ", "_")}`} key={category.id} render={(props) => ( <EventPage {...props} categoryName={category.name} categoryId={category.id} />)} exact={true} />;
-                                    })
-                                }
                                 
-                                <Route path="/events" component={EventsPage} exact={true} />
-                                <Route path="/signin" component={SigninPage} exact={true} />
-                                <PublicRoute path="/login" component={LoginPage} exact={true} />
-                                <Route path="/קצת_אחרת" component={DifferentPage} exact={true} />
                                 <Route component={NotFoundPage} />
                             </Switch>
                             :
@@ -121,3 +103,23 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps)(AppRouter);
+
+// <Route path="/about" component={AboutPage} exact={true} />
+//                                 <Route path="/contact" component={ContactPage} exact={true} />
+                            
+                                    
+//                                 {
+//                                     this.props.events.categories.map((category, index) => {
+//                                         return <Route path={`/${category.name.replace(" ", "_").replace(" ", "_").replace(" ", "_").replace(" ", "_")}`} key={category.id} render={(props) => ( <EventsPage {...props} category={category} categoryIndex={index} />)} exact={true} />;
+//                                     })
+//                                 }
+//                                 {
+//                                     this.props.events.categories.map((category) => {
+//                                         return <Route path={`/:event/${category.name.replace(" ", "_").replace(" ", "_").replace(" ", "_").replace(" ", "_")}`} key={category.id} render={(props) => ( <EventPage {...props} categoryName={category.name} categoryId={category.id} />)} exact={true} />;
+//                                     })
+//                                 }
+                                
+//                                 <Route path="/events" component={EventsPage} exact={true} />
+//                                 <Route path="/signin" component={SigninPage} exact={true} />
+//                                 <PublicRoute path="/login" component={LoginPage} exact={true} />
+//                                 <Route path="/קצת_אחרת" component={DifferentPage} exact={true} />
