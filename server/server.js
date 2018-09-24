9a2491a44b507f465d104273bef246f854100c17
oren-pro <mosh.kainer@gmail.com>
2018-09-24 22:44:20 +0300

    sitemap.xml 1

--- server/server.js
+++ server/server.js
@@ -154,11 +154,54 @@ const publicPath = path.join(__dirname, '../', 'public');
 const port = process.env.PORT || 3000;
 
 
+// robots.txt
 
 app.get('/robots.txt', function (req, res) {
     res.type('text/plain');
     res.send("User-agent: *\nDisallow: /login\nAllow: /");
 });
+
+
+// sitemap.xml
+
+function generate_xml_sitemap() {
+    // this is the source of the URLs on your site, in this case we use a simple array, actually it could come from the database
+    let urls = [];
+    // the root of your website - the protocol and the domain name with a trailing slash
+    var root_path = 'https://oren-pro-website.herokuapp.com/';
+
+    var db = admin.database();
+    var ref = db.ref('eventsCategories/');
+    ref.once("value", function(snapshot) {
+      //console.log(snapshot.val());
+        if(snapshot.val() !== null) {
+          const categories = snapshot.val();
+          categories.map((category, index) => {
+            urls.push(category);
+          });
+
+          var priority = 0.5;
+          var freq = 'monthly';
+          var xml = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+          for (var i in urls) {
+              xml += '<url>';
+              xml += '<loc>'+ root_path + urls[i] + '</loc>';
+              xml += '<changefreq>'+ freq +'</changefreq>';
+              xml += '<priority>'+ priority +'</priority>';
+              xml += '</url>';
+              i++;
+          }
+          xml += '</urlset>';
+          return xml;
+        }
+    });
+}
+
+app.get('/sitemap.xml', function(req, res) {
+    var sitemap = generate_xml_sitemap(); // get the dynamically generated XML sitemap
+    res.header('Content-Type', 'text/xml');
+    res.send(sitemap);     
+})
 
 
 
