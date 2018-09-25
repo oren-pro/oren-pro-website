ec78a9ffd112d514015083777039f10fbfcd731f
oren-pro <mosh.kainer@gmail.com>
2018-09-25 21:14:23 +0300

    sitemap.xml 17

--- server/server.js
+++ server/server.js
@@ -227,6 +227,7 @@ app.get('/sitemap.xml', function(req, res) {
     //var sitemap = '';
     console.log(sitemap);
     res.header('Content-Type', 'text/xml');
+    console.log('sending sitemap');
     res.send(sitemap);     
 })
 
