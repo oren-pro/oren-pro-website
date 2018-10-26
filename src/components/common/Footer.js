41f72113660521acaf306a5519c0b006e7396b81
oren-pro <mosh.kainer@gmail.com>
2018-10-26 18:47:15 +0300

    iphone scroll fix 18

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () => (
     <div className="footer__box">
         <p className="footer__text Heebo-Regular desktop" dir="rtl">
-            כל הזכויות שמורות לאורן ורינת | עיצוב גרפי <a className="footer__link" href="http://hollestudio.co.il/" target="_BLANK">HolleStudio</a>| תכנות frixell | כתיבה שיווקית יפעת בלפורד 
+            כל הזכויות שמורות לאורן ורינת | עיצוב גרפי <a className="footer__link" href="http://hollestudio.co.il/" target="_BLANK">HolleStudio</a> | תכנות frixell | כתיבה שיווקית יפעת בלפורד 
         </p>
         <p className="footer__text Heebo-Regular mobile" dir="rtl">
-            כל הזכויות שמורות לאורן ורינת <br /> עיצוב גרפי <a className="footer__link" href="http://hollestudio.co.il/" target="_BLANK">HolleStudio</a> | תכנות frixell<br />| כתיבה שיווקית יפעת בלפורד 
+            כל הזכויות שמורות לאורן ורינת <br /> עיצוב גרפי <a className="footer__link" href="http://hollestudio.co.il/" target="_BLANK">HolleStudio</a> | תכנות frixell<br /> כתיבה שיווקית יפעת בלפורד 
         </p>
     </div>
 );
