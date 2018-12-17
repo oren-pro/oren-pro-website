b8b43abc4a19b1c5fe42f86a3b7c0f387f62caa8
oren-pro <mosh.kainer@gmail.com>
2018-12-17 18:03:12 +0200

    articls

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => (
     <div className="footer__box">
         <p className="footer__text Heebo-Regular desktop" dir="rtl">
-            כל הזכויות שמורות לאורן ורינת | עיצוב גרפי <a className="footer__link" href="http://hollestudio.co.il/" target="_BLANK">HolleStudio</a> | תכנות frixell | כתיבה שיווקית יפעת בלפורד 
+            
+            <NavLink to="/מאמרים">מאמרים</NavLink> | כל הזכויות שמורות לאורן ורינת | עיצוב גרפי <a className="footer__link" href="http://hollestudio.co.il/" target="_BLANK">HolleStudio</a> | תכנות frixell | כתיבה שיווקית יפעת בלפורד 
         </p>
         <p className="footer__text Heebo-Regular mobile" dir="rtl">
-            כל הזכויות שמורות לאורן ורינת <br /> עיצוב גרפי <a className="footer__link" href="http://hollestudio.co.il/" target="_BLANK">HolleStudio</a> | תכנות frixell<br /> כתיבה שיווקית יפעת בלפורד 
+            <NavLink to="/מאמרים">מאמרים</NavLink> | כל הזכויות שמורות לאורן ורינת <br /> עיצוב גרפי <a className="footer__link" href="http://hollestudio.co.il/" target="_BLANK">HolleStudio</a> | תכנות frixell<br /> כתיבה שיווקית יפעת בלפורד 
         </p>
     </div>
 );
