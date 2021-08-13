94dffda513412be53260b945daa0aedf27fd8b7a
mosh kainer <mosh.kainer@gmail.com>
2021-08-13 22:32:45 +0300

    force https://www.oren-pro.com 4

--- server/server.js
+++ server/server.js
@@ -15,8 +15,7 @@ app.use(function forceHTTPS(req, res, next) {
   var local = req.url;
   var schema = (req.headers['x-forwarded-proto'] || '').toLowerCase();
   var www = req.headers.host;
-  //  || www.indexOf('www') < 0
-  if (schema !== 'https') {
+  if (schema !== 'https' || www.indexOf('www') < 0) {
     return res.redirect('https://www.oren-pro.com' + local);
   }
 
